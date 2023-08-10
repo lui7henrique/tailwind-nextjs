a9949c9689a521b734a5cd0f3b9d1625b0ac73ba
Luiz Henrique <7henrique18@gmail.com>
2023-08-10 20:39:14 -0300

    feat: Form initial structure

--- src/components/SettingsTabs/TabItem.tsx
+++ src/components/SettingsTabs/TabItem.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import * as Tabs from '@radix-ui/react-tabs'
+import { motion } from 'framer-motion'
 
 export interface TabItemProps {
   value: string
@@ -14,10 +15,13 @@ export function TabItem({ value, title, isSelected = false }: TabItemProps) {
       value={value}
       className="relative px-1 pb-4 text-sm font-medium text-zinc-500 hover:text-violet-700 data-[state=active]:text-violet-700"
     >
-      <span>{title}</span>
+      <span className="whitespace-nowrap">{title}</span>
 
       {isSelected && (
-        <div className="absolute -bottom-px left-0 right-0 h-0.5 bg-violet-700" />
+        <motion.div
+          layoutId="activeTab"
+          className="absolute -bottom-px left-0 right-0 h-0.5 bg-violet-700"
+        />
       )}
     </Tabs.Trigger>
   )
