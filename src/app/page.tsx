71ff769304f532fbe76e19d227954abda5373537
Luiz Henrique <7henrique18@gmail.com>
2023-08-10 21:09:29 -0300

    feat: Files list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -145,6 +145,7 @@ export default function Home() {
 
             <FileInput.Root>
               <FileInput.Trigger />
+              <FileInput.FileList />
               <FileInput.Control multiple />
             </FileInput.Root>
             <div />
